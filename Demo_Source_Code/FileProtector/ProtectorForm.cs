5067ead7eec966e672ab46f63c7cfe8cec412404
victor <support@easefilter.com>
2022-12-05 10:40:41 -0500

    Update ProtectorForm.cs

--- Demo_Source_Code/FileProtector/ProtectorForm.cs
+++ Demo_Source_Code/FileProtector/ProtectorForm.cs
@@ -37,7 +37,7 @@ namespace FileProtector
     {
         //Purchase a license key with the link: http://www.easefilter.com/Order.htm
         //Email us to request a trial key: info@easefilter.com //free email is not accepted.
-        string licenseKey = "27AE4-2EC94-6E533-C186E-C3EC0-50CD9";
+        string licenseKey = "****************************************************";
 
         MonitorEventHandler monitorEventHandler = null;
         ControlEventHandler controlEventHandler = null;
